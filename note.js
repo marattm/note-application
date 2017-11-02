11dc36a0e73f1d6f785da79be8ba05b67e15bd86
marattm <mtulepbayevmonnie@hawk.iit.edu>
2017-11-02 02:35:29 -0500

    functionnal version, missing : script injection ckeck, date stamp, collapside

--- note.js
+++ note.js
@@ -2,13 +2,13 @@ jQuery(document).ready(function($){
 	//console.log('note.js loaded');
 	var db;
 
-	var openRequest = indexedDB.open('contacts', 1);
+	var openRequest = indexedDB.open('notes', 1);
 
 	openRequest.onupgradeneeded = function(e){
 		console.log('Upgrading DB...');
 		var thisDB = e.target.result;
-		if(!thisDB.objectStoreNames.contains('contactstore')) {
-			thisDB.createObjectStore('contactstore', { autoIncrement : true });
+		if(!thisDB.objectStoreNames.contains('notestore')) {
+			thisDB.createObjectStore('notestore', { autoIncrement : true });
 		}
 	};
 
@@ -16,17 +16,20 @@ jQuery(document).ready(function($){
 		console.log('Open Success!');
 		db = e.target.result;
 		$('#save-btn').click(function(e){
-			var nameIn = $('#fullname').val();
-			var emailIn = $('#email').val();
+			var nameIn = $('#notename').val();
+			var subjectIn = $('#subject').val();
+			// creating a new input : note texte
+			var noteIn = $('#note').val();
 			if (!nameIn.trim()) {
-				alert('Full Name is Required!');
-			} else if (!emailIn.trim()) {
-				alert('Email is Required!');
+				alert('Author is Required!');
+			} else if (!subjectIn.trim()) {
+				alert('Subject is Required!');
 			} else {
-				addContact(new Contact(nameIn, emailIn));
+				addNote(new Note(nameIn, subjectIn,noteIn));
 				$('#new-form').toggle();
-				$('#fullname').val('');
-				$('#email').val('');
+				$('#notename').val('');
+				$('#subject').val('');
+				console.log('New Note added.');
 			}
 		});
 		renderList();
@@ -37,42 +40,51 @@ jQuery(document).ready(function($){
 		console.dir(e);
 	}
 
-	// Setup new contact buttons
+	// Setup new note buttons
 	$('#new-btn').click(function(e){
 		$('#new-form').toggle();
 	});
+	// Setup new note buttons
+	$('#mynote').click(function(e){
+		$('#tables').toggle();
+	});
+	 $(function() {
+		$('select').material_select();
+	 });
 	  
 
 	// Render List Function
 	function renderList(){
 		$('#list-wrapper').empty();
-		$('#list-wrapper').html('<table><tr><th>Key</th><th>Full Name</th><th>Email Address</th></tr></table>');
+		$('#list-wrapper').html('<table><tr><th>Key</th><th>Author</th><th>Subject </th><th>Note</th></tr></table>');
 
 		//Count Objects
-		var transaction = db.transaction(['contactstore'], 'readonly');
-		var store = transaction.objectStore('contactstore');
+		var transaction = db.transaction(['notestore'], 'readonly');
+		var store = transaction.objectStore('notestore');
 		var countRequest = store.count();
 		countRequest.onsuccess = function(){
 			console.log(countRequest.result)
 			var count = Number(countRequest.result);
-			// Get all Objects and display if contacts exist
+			// Get all Objects and display if notes exist
 			if (count > 0) {
-				var objectStore = db.transaction(['contactstore'], 'readonly').objectStore('contactstore');
+				var objectStore = db.transaction(['notestore'], 'readonly').objectStore('notestore');
 				objectStore.openCursor().onsuccess = function(e){
 					var cursor = e.target.result;
 					if (cursor) {
 						var $row = $('<tr>');
 						var $keyCell = $('<td>' + cursor.key + '</td>');
-						var $nameLink = $('<a href="#" data-key="' + cursor.key + '">' + cursor.value.fullname + '</a>');
+						var $nameLink = $('<a href="#" data-key="' + cursor.key + '">' + cursor.value.notename + '</a>');
 						$nameLink.click(function(){
 							//alert('Clicked ' + $(this).attr('data-key'));
-							loadContactByKey(Number($(this).attr('data-key')));
+							loadNoteByKey(Number($(this).attr('data-key')));
 						});
 						var $nameCell = $('<td></td>').append($nameLink);
-						var $emailCell = $('<td></td>').append(cursor.value.email);
+						var $subjectCell = $('<td></td>').append(cursor.value.subject);
+						var $noteCell = $('<td class="content hidecontent"></td>').append(cursor.value.note);
 						$row.append($keyCell);
 						$row.append($nameCell);
-						$row.append($emailCell);
+						$row.append($subjectCell);
+						$row.append($noteCell);
 						$('#list-wrapper table').append($row);
 						cursor.continue();
 					}
@@ -82,39 +94,41 @@ jQuery(document).ready(function($){
 				};
 			} else {
 				$('#list-wrapper').empty();
-				$('#list-wrapper').html('<h3>No Contacts to show!</h3>');
+				$('#list-wrapper').html('<h3>No Note to show!</h3>');
 			}
 		};
 	} //end renderList()
 
-	// Add new contact function
-	function addContact(contact){
-		console.log('adding ' + contact.fullname);
-		var transaction = db.transaction(['contactstore'],'readwrite');
-		var store = transaction.objectStore('contactstore');
-		var addRequest = store.add(contact);
+	// Add new note function
+	function addNote(note){
+		console.log('adding ' + note.notename);
+		var transaction = db.transaction(['notestore'],'readwrite');
+		var store = transaction.objectStore('notestore');
+		var addRequest = store.add(note);
 		addRequest.onerror = function(e) {
 			console.log("Error", e.target.error.name);
 	        //some type of error handler
 	    }
 	    addRequest.onsuccess = function(e) {
-	    	console.log("added " + contact.fullname);
-	    	$('#fullname').val('');
-			$('#email').val('');
+	    	console.log("added " + note.notename);
+	    	$('#notename').val('');
+			$('#subject').val('');
+			$('#note').val('');
 	    	renderList();   	
 	    }
-	} //end addContact()
+	} //end addNote()
 
-	// Create contact data model
-	function Contact(fullname, email){
-		this.fullname = fullname;
-		this.email = email;
+	// Create note data model
+	function Note(notename, subject,note){
+		this.notename = notename;
+		this.subject = subject;
+		this.note = note;
 	}
 
 	// Load by key function
-	function loadContactByKey(k){
-		var transaction = db.transaction(['contactstore'], 'readonly');
-		var store = transaction.objectStore('contactstore');
+	function loadNoteByKey(k){
+		var transaction = db.transaction(['notestore'], 'readonly');
+		var store = transaction.objectStore('notestore');
 		var request = store.get(k);
 
 		request.onerror = function(e) {
@@ -123,54 +137,61 @@ jQuery(document).ready(function($){
 		request.onsuccess = function(e) {
 			// Do something with the request.result!
 			console.log(request);
-			$('#detail').html('<h2>Show contact ' + request.result.fullname + '</h2>');
-			$('#detail').append($('<label>Full Name: <input type="text" id="fullname-detail" value="' + request.result.fullname + '"></label><br>'));
-			$('#detail').append($('<label>Email: <input type="email" id="email-detail" value="' + request.result.email + '"></label><br>'));
-			var $delBtn = $('<button>Delete ' + request.result.fullname + '</button>');
+			$('#detail').html('<h2>' + request.result.notename + '</h2>');
+			$('#detail').append($('<p><label>Author <input type="text" id="notename-detail" value="' + request.result.notename + '"></label></p>'));
+			$('#detail').append($('<p/><label>Subject<input type="text" id="subject-detail" value="' + request.result.subject + '"></label></p>'));
+			$('#detail').append($('<p><label>Note<textarea id="note" type="text"rows="100" cols="50" class="materialize-textarea">' + request.result.note + '</textarea></p>'));
+
+			
+
+			var $delBtn = $('<button class="btn right">Delete ' + request.result.notename + '</button>');
 			$delBtn.click(function(){
 		   		console.log('Delete ' + k);
-		   		deleteContact(k);
+		   		deleteNote(k);
 			});
-			var $saveBtn = $('<button>Save Changes</button>');
+			var $saveBtn = $('<button class="btn left">Save Changes</button>');
 			$saveBtn.click(function(){
 				console.log('update ' + k);
-				updateContact(k);
+				updateNote(k);
 			});
 			$('#detail').append($delBtn);
 			$('#detail').append($saveBtn);
+			$('#detail').append('<p></p>');
 			$('#detail').show();
 		};
-	} // end loadContactByKey()
+	} // end loadNoteByKey()
 
 	// Delete by key
-	function deleteContact(k) {
-		var transaction = db.transaction(['contactstore'], 'readwrite');
-		var store = transaction.objectStore('contactstore');
+	function deleteNote(k) {
+		var transaction = db.transaction(['notestore'], 'readwrite');
+		var store = transaction.objectStore('notestore');
 		var request = store.delete(k);
 		request.onsuccess = function(e){
 			renderList();
 			$('#detail').empty();
 			$('#detail').hide();
 		};
-	} // end deleteContact()
+	} // end deleteNote()
 
-	// Update contact
-	function updateContact(k) {
-		var nameIn = $('#fullname-detail').val();
-		var emailIn = $('#email-detail').val();
+	// Update note
+	function updateNote(k) {
+		var nameIn = $('#notename-detail').val();
+		var subjectIn = $('#subject-detail').val();
 		if (!nameIn.trim()) {
-			alert('Full Name is Required!');
-		} else if (!emailIn.trim()) {
-			alert('Email is Required!');
+			alert('Author is Required!');
+		} else if (!subjectIn.trim()) {
+			alert('Subject is Required!');
+		} else if (!noteIn.trim()) {
+			alert('Note is Required!');
 		} else {
-			var contact = new Contact(nameIn, emailIn);
-			var transaction = db.transaction(['contactstore'], 'readwrite');
-			var store = transaction.objectStore('contactstore');
-			var request = store.put(contact, k);
+			var note = new Note(nameIn, subjectIn,noteIn);
+			var transaction = db.transaction(['notestore'], 'readwrite');
+			var store = transaction.objectStore('notestore');
+			var request = store.put(note, k);
 			renderList();
 			$('#detail').empty();
 			$('#detail').hide();
 		}
-	} // end updateContact()
+	} // end updateNote()
 
 }); //end document ready function
