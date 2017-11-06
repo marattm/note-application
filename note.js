8b32828e95e22107e20a50164516cd14b6b9e98f
marattm <mtulepbayevmonnie@hawk.iit.edu>
2017-11-05 20:41:29 -0600

    side nav added

--- note.js
+++ note.js
@@ -286,4 +286,11 @@ jQuery(document).ready(function($){
 		return encodedMsg;
 	  };
 
+	$('.button-collapse').sideNav({
+		menuWidth: 300, // Default is 240
+		closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+	}
+	);
+	$('.collapsible').collapsible();
+
 }); //end document ready function
